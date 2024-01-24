85608b67e29742c19a6c66db8572f439f94fb3d7
John <johncruzag@gmail.com>
2024-01-23 20:11:41 -0500

    Before Checkout for Subscription implementation

---
+++ components/CreditCardForm.tsx
@@ -0,0 +1,66 @@
+import {
+  useElements,
+  useStripe,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
+import { useState } from "react";
+
+const CreditCardForm = () => {
+  const elements = useElements();
+  const stripe = useStripe();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const onClick = async () => {
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const { paymentIntent, error } = await stripe.confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: "http://localhost:3000",
+        },
+        redirect: "if_required",
+      });
+      if (error) {
+        throw error.message;
+      }
+      if (paymentIntent.status === "succeeded") {
+        alert(
+          "Payment success. The NFT will be delivered to your wallet shortly."
+        );
+        setIsCompleted(true);
+      } else {
+        alert("Payment failed. Please try again.");
+      }
+    } catch (e) {
+      alert(`There was an error with the payment. ${e}`);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <PaymentElement />
+
+      <button
+        className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-lg w-full"
+        onClick={onClick}
+        disabled={isLoading || isCompleted || !stripe || !elements}
+      >
+        {isCompleted
+          ? "Payment received"
+          : isLoading
+          ? "Please wait..."
+          : "Pay now"}
+      </button>
+    </>
+  );
+};
+
+export default CreditCardForm
